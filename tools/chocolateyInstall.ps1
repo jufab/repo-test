f71c00bf6ed0bdf1eae3380400e4d7da3e8f40fb
Julien FABRE <julien.fabre.33@gmail.com>
2023-01-24 00:54:09 +0100

    1.0.22

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$checksum = "tototototo"
+$checksum = "83a26287f39b1a96eb907bbcadd4770a1bf3b02b702370d01b21d4490445e9bf"
 packageName = 'jenkins-x'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $nuspecPath = Resolve-Path -Path $toolsPath+'\..'
